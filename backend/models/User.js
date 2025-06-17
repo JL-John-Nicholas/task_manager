4681a8cec90a3247dda9c21792384103bddeb7c5
JL-John-Nicholas <jljohnnicholas@gmail.com>
2025-06-17 20:29:17 +0530

    modified user schema and added name attributes in index.html

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-  username: { type: String, unique: true },
-  password: String
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
 });
 
 UserSchema.pre('save', async function(next) {
